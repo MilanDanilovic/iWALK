92416bbcdfa96f8a6fdec0e528007e819b77ef30
Jana <jana.rajkovic@mitaway.com>
2022-06-02 17:18:51 +0200

    prikazivanje ljubimaca, add ljubimaca, i popuna podataka o ljubimcu kod edita

--- iWALK/app/src/main/java/elfak/mosis/iwalk/EditPetFragment.kt
+++ iWALK/app/src/main/java/elfak/mosis/iwalk/EditPetFragment.kt
@@ -2,17 +2,22 @@ package elfak.mosis.iwalk
 
 import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class EditPetFragment : Fragment() {
+
+    private lateinit var name : EditText
+    private lateinit var breed : EditText
+    private lateinit var weight : EditText
+    private lateinit var description : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,25 @@ class EditPetFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        name = requireView().findViewById<EditText>(R.id.pet_edit_name_value)
+        breed = requireView().findViewById<EditText>(R.id.pet_edit_breed_value)
+        weight = requireView().findViewById<EditText>(R.id.pet_edit_weight_value)
+        description = requireView().findViewById<EditText>(R.id.pet_edit_description_value)
+
+        val bundle = this.arguments
+        if (bundle != null) {
+            name.setText(bundle.getString("pet_name"))
+            breed.setText(bundle.getString("pet_breed"))
+            weight.setText(bundle.getString("pet_weight"))
+            description.setText(bundle.getString("pet_description"))
+            //val myInt = bundle.getInt(key, defaultValue)
+        }
+        /*name.setText(activity?.getIntent()?.getExtras()?.getString("pet_name"))
+        breed.setText(activity?.getIntent()?.getExtras()?.getString("pet_breed"))
+        weight.setText(activity?.getIntent()?.getExtras()?.getString("pet_weight"))
+        description.setText(activity?.getIntent()?.getExtras()?.getString("pet_description"))*/
+        //description.setText(resultList[3])
 
         val cancel: Button = requireView().findViewById<Button>(R.id.button_cancel_edit_pet)
         val edit: Button = requireView().findViewById<Button>(R.id.button_edit_pet)
@@ -29,10 +53,10 @@ class EditPetFragment : Fragment() {
             alertDialog.setMessage("Are you sure you want to cancel changes?")
 
             alertDialog.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
-                val fragment: Fragment = ProfileFragment()
+                val fragment: Fragment = MyPetsFragment()
                 val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
                 val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.edit_profile_fragment, fragment)
+                fragmentTransaction.replace(R.id.edit_pet_fragment, fragment)
                 fragmentTransaction.addToBackStack(null)
                 fragmentTransaction.commit()
             })
@@ -45,10 +69,10 @@ class EditPetFragment : Fragment() {
             alertDialog.setMessage("Are you sure you want to save changes?")
 
             alertDialog.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
-                val fragment: Fragment = ProfileFragment()
+                val fragment: Fragment = MyPetsFragment()
                 val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
                 val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.edit_profile_fragment, fragment)
+                fragmentTransaction.replace(R.id.edit_pet_fragment, fragment)
                 fragmentTransaction.addToBackStack(null)
                 fragmentTransaction.commit()
             })
