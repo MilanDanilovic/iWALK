c27eda4ccf5a90f2f96533b6068ad2ebb7654b65
janana <jana.rajkovic@elfak.rs>
2022-06-16 15:15:53 +0200

    ne radi dodavanje prijatelja

--- iWALK/app/src/main/java/elfak/mosis/iwalk/CustomGridFindFriendsFragment.kt
+++ iWALK/app/src/main/java/elfak/mosis/iwalk/CustomGridFindFriendsFragment.kt
@@ -1,6 +1,8 @@
 package elfak.mosis.iwalk
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,9 +12,12 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsClient.getPackageName
 import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
@@ -44,6 +49,7 @@ class CustomGridFindFriendsFragment : Fragment() {
         val usersSenderRef: CollectionReference = db.collection("users")
         val usersReceiverRef: CollectionReference = db.collection("users")
         val friendRequests: CollectionReference = db.collection("friendRequests")
+        val usersRef = db.collection("users")
         auth = Firebase.auth
 
         val bundle = this.arguments
@@ -61,6 +67,11 @@ class CustomGridFindFriendsFragment : Fragment() {
                         if (document["receiver"].toString() == userId && document["sender"].toString() == auth.currentUser!!.uid) {
                             addFriend.setImageDrawable(resources.getDrawable(R.drawable.ic_cancel_changes))
                             requestAlreadySent = "true"
+                            break
+                        }
+                        else if (document["receiver"].toString() == auth.currentUser!!.uid && document["sender"].toString() ==userId) {
+                            addFriend.setImageDrawable(resources.getDrawable(R.drawable.ic_save_changes))
+                            requestAlreadySent = "sentMe"
                             break
                         }
                         else {
@@ -184,11 +195,136 @@ class CustomGridFindFriendsFragment : Fragment() {
                         }
                     }
             }
+            else if (requestAlreadySent == "sentMe") {
+                usersReceiverRef.get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            for (document in task.result) {
+                                if (document.id == userId) { //DODAJEM KOD PRETRAZENOG USER-A
+
+                                    val query = usersRef.whereEqualTo("username", document["username"])
+                                    query.get().addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                            val dataToSave: MutableMap<Array<String>, Any> =
+                                                HashMap()
+                                            val friend = mutableMapOf(
+                                                "friendId" to auth.currentUser!!.uid
+                                            )
+
+                                            dataToSave[arrayOf("friends")] = friend
+
+                                            val documentReference = db.collection("users").document(userId)
+                                            documentReference.update(dataToSave)
+                                                .addOnCompleteListener { task ->
+                                                    if (task.isSuccessful) {
+                                                        Toast.makeText(
+                                                            context,
+                                                            "Profile is successfully updated.",
+                                                            Toast.LENGTH_SHORT
+                                                        ).show()
+                                                    } else {
+                                                        Toast.makeText(
+                                                            context,
+                                                            "Error while updating data!",
+                                                            Toast.LENGTH_SHORT
+                                                        ).show()
+                                                    }
+                                                }.addOnFailureListener { }
+                                        }
+                                    }
+                                    /*
+
+                                    usersRef.whereArrayContains("friends", friend).get().addOnCompleteListener{ task ->
+                                        task.apply {
+                                            if (task.isSuccessful) {
+                                                for (documentFriends in result) {
+                                                    val docIdRef = usersRef.document(documentFriends.id)
+                                                    docIdRef.update("friends", FieldValue.arrayUnion(friend)).addOnCompleteListener{ add ->
+                                                        if (add.isSuccessful) {
+                                                            docIdRef.update("friends", FieldValue.arrayUnion(FriendshipFriend(auth.currentUser!!.uid))).addOnCompleteListener{ additionTask ->
+                                                                if (additionTask.isSuccessful) {
+                                                                    Toast.makeText(
+                                                                        context,
+                                                                        "Update complete! ",
+                                                                        Toast.LENGTH_LONG
+                                                                    ).show()
+                                                                    Log.w("TAG", "Friend request is not saved in database! ")
+                                                                } else {
+                                                                    additionTask.exception?.message?.let {
+                                                                        Log.e("TAG", it)
+                                                                    }
+                                                                }
+                                                            }
+                                                        } else {
+                                                            add.exception?.message?.let {
+                                                                Log.e("TAG", it)
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            } else {
+                                                task.exception?.message?.let {
+                                                    Log.e("TAG", it)
+                                                }
+                                            }
+                                        }
+                                    }*/
+                                }
+                                if (document.id == auth.currentUser!!.uid) {
+
+                                    /*val friend = mutableMapOf(
+                                        "friendId" to userId
+                                    )
+
+                                    usersRef.whereArrayContains("friends", friend).get().addOnCompleteListener{ task ->
+                                        task.apply {
+                                            if (task.isSuccessful) {
+                                                for (documentFriends in result) {
+                                                    val docIdRef = usersRef.document(documentFriends.id)
+                                                    docIdRef.update("friends", FieldValue.arrayUnion(friend)).addOnCompleteListener{ add ->
+                                                        if (add.isSuccessful) {
+                                                            docIdRef.update("friends", FieldValue.arrayUnion(FriendshipFriend(userId))).addOnCompleteListener{ additionTask ->
+                                                                if (additionTask.isSuccessful) {
+                                                                    Toast.makeText(
+                                                                        context,
+                                                                        "Update complete! ",
+                                                                        Toast.LENGTH_LONG
+                                                                    ).show()
+                                                                    Log.w("TAG", "Friend request is not saved in database! ")
+                                                                } else {
+                                                                    additionTask.exception?.message?.let {
+                                                                        Log.e("TAG", it)
+                                                                    }
+                                                                }
+                                                            }
+                                                        } else {
+                                                            add.exception?.message?.let {
+                                                                Log.e("TAG", it)
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            } else {
+                                                task.exception?.message?.let {
+                                                    Log.e("TAG", it)
+                                                }
+                                            }
+                                        }
+                                    }*/
+                                }
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.exception)
+                        }
+                    }
+            }
         })
     }
 
-    private fun sendFriendRequest() {
-        TODO("Not yet implemented")
+    fun append(arr: Array<String>, element: String): Array<String> {
+        val list: MutableList<String> = arr.toMutableList()
+        list.add(element)
+        return list.toTypedArray()
     }
 
     override fun onCreateView(
