1e405d09bbe6913dd8901dfd5a3739d6788899b3
Milan <mdanilovic06@gmail.com>
2022-08-25 20:01:04 +0200

    My Location added to map

--- iWALK/app/src/main/java/elfak/mosis/iwalk/map/MapFragment.kt
+++ iWALK/app/src/main/java/elfak/mosis/iwalk/map/MapFragment.kt
@@ -1,9 +1,13 @@
 package elfak.mosis.iwalk.map
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,7 +18,36 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var googleMap: GoogleMap
+    private lateinit var map: GoogleMap
+    private val LOCATION_PERMISSION_REQUEST=1
+
+    @SuppressLint("MissingPermission")
+    private fun getLocationAccess(){
+        if (requireActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && activity?.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+            map.isMyLocationEnabled = true
+        }
+            else
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if(grantResults.contains(PackageManager.PERMISSION_GRANTED)){
+                map.isMyLocationEnabled = true
+            }
+            else {
+                Toast.makeText(activity,"User has not granted location access permission", Toast.LENGTH_SHORT).show()
+                requireActivity().finish()
+                //mozda ne radi
+            }
+        }
+    }
+
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -25,9 +58,10 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         binding.mapView.getMapAsync(this)
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        map?.let {
-            googleMap = it
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap?.let {
+            map = it
+            getLocationAccess()
         }
     }
 
