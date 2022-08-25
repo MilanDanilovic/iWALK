4c06e333b333b0693f8aa6c5522e24982855b6e7
Milan <mdanilovic06@gmail.com>
2022-08-25 23:17:45 +0200

    Real time tracking of users location

--- iWALK/app/src/main/java/elfak/mosis/iwalk/map/MapFragment.kt
+++ iWALK/app/src/main/java/elfak/mosis/iwalk/map/MapFragment.kt
@@ -3,14 +3,19 @@ package elfak.mosis.iwalk.map
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import elfak.mosis.iwalk.R
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.*
 import elfak.mosis.iwalk.databinding.FragmentMapBinding
 
 class MapFragment : Fragment(), OnMapReadyCallback {
@@ -18,16 +23,84 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
+    private var isCameraInitiallySet: Boolean = false
     private lateinit var map: GoogleMap
     private val LOCATION_PERMISSION_REQUEST=1
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    private var currentUserMarker: Marker? = null
+    private val listOfOtherUsersMarkers = ArrayList<Marker>()
 
     @SuppressLint("MissingPermission")
     private fun getLocationAccess(){
         if (requireActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && activity?.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
-            map.isMyLocationEnabled = true
+            map.isMyLocationEnabled = true //TODO Check if we want to have option to access current location everytime
+            getLocationUpdates()
+            startLocationUpdates()
         }
             else
                 requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST)
+    }
+
+    private fun getLocationUpdates() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 15000
+        locationRequest.fastestInterval = 10000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                if(locationResult.locations.isNotEmpty()){
+                    val lastLocation = locationResult.lastLocation
+                    if(lastLocation != null){
+                        val latLng = LatLng(lastLocation.latitude,lastLocation.longitude)
+
+                        val bearing = lastLocation.bearing
+                        if(!isCameraInitiallySet) { //TODO Check if we want to have camera fixed to our marker
+                            map.moveCamera(
+                                CameraUpdateFactory.newCameraPosition(
+                                    CameraPosition.Builder()
+                                        .target(
+                                            LatLng(
+                                                lastLocation.latitude,
+                                                lastLocation.longitude
+                                            )
+                                        )
+                                        .bearing(bearing)
+                                        .zoom(18f)
+                                        .tilt(30f)
+                                        .build()
+                                )
+                            )
+                            isCameraInitiallySet = true
+                        }
+
+                        currentUserMarker?.remove()
+                        currentUserMarker = map.addMarker(MarkerOptions()
+                            .position(LatLng(lastLocation.latitude,lastLocation.longitude))
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.user_location_marker))
+                            .flat(true)
+                            .anchor(0.5f,0.5f)
+                            .rotation(lastLocation.bearing))
+
+//                        val markerOptions = MarkerOptions().position(latLng)
+//                        map.addMarker(markerOptions)
+//                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates(){
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
     }
 
     @SuppressLint("MissingPermission")
@@ -42,7 +115,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             else {
                 Toast.makeText(activity,"User has not granted location access permission", Toast.LENGTH_SHORT).show()
                 requireActivity().finish()
-                //mozda ne radi
             }
         }
     }
@@ -56,6 +128,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         binding.mapView.onResume()
 
         binding.mapView.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
