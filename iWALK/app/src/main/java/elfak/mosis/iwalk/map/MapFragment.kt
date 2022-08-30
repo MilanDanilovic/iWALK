81ff2f57322408404c9a01ebc3ded0aa4ed099a4
Jana <jana.rajkovic@mitaway.com>
2022-08-30 20:50:04 +0200

    dodavanje i brisanje markera

--- iWALK/app/src/main/java/elfak/mosis/iwalk/map/MapFragment.kt
+++ iWALK/app/src/main/java/elfak/mosis/iwalk/map/MapFragment.kt
@@ -27,6 +27,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import elfak.mosis.iwalk.AdapterMyPets
@@ -249,15 +250,13 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         for (document in task.result) {
-                            if (document.getString("userId") == auth.currentUser?.uid && document.getString("latitude") == markerToDelete.position.latitude.toString() && document.getString("longitude") == markerToDelete.position.longitude.toString()) {
-                                docRef.collection("markers")
-                                    .document()
+                            if (document["userId"] == auth.currentUser?.uid && document["latitude"] == markerToDelete.position.latitude && document["longitude"] == markerToDelete.position.longitude) {
+                                val markersRefDelete: DocumentReference = docRef.collection("markers").document(document.id.toString())
+                                markersRefDelete
                                     .delete()
                                     .addOnCompleteListener { task ->
                                         if (task.isSuccessful) {
-                                            docRef.collection("markers").document(
-                                                document.id
-                                            )
+                                            docRef.collection("markers").document(document.id)
                                                 .delete()
                                                 .addOnSuccessListener {
                                                     Log.d(
@@ -272,8 +271,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                                                         e
                                                     )
                                                 }
-                                            //markers.remove(markerToDelete)
-                                            //markerToDelete.remove()
+                                            markers.remove(markerToDelete)
+                                            markerToDelete.remove()
                                         } else {
                                             Log.w(
                                                 "TAG",
