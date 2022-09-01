c34e89260f7b24c56cb35ceaec34f8a1b6377bf2
janana <jana.rajkovic@elfak.rs>
2022-09-01 16:13:12 +0200

    pocela sam finish dugme da radim

--- iWALK/app/src/main/java/elfak/mosis/iwalk/AdapterInProgressWalks.kt
+++ iWALK/app/src/main/java/elfak/mosis/iwalk/AdapterInProgressWalks.kt
@@ -5,16 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
+
 
 class AdapterInProgressWalks(var ctx: Context, walksList: MutableList<Walks>) :
     RecyclerView.Adapter<WalksHolder>() {
@@ -22,6 +22,8 @@ class AdapterInProgressWalks(var ctx: Context, walksList: MutableList<Walks>) :
     lateinit var walksList: MutableList<Walks>
     var baseAuth: FirebaseAuth? = null
     private val docRef = FirebaseFirestore.getInstance()
+    var popUp: PopupWindow? = null
+    var click = true
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): WalksHolder {
         val mView: View = LayoutInflater.from(viewGroup.context)
@@ -55,8 +57,71 @@ class AdapterInProgressWalks(var ctx: Context, walksList: MutableList<Walks>) :
                 }
         }
 
+        walksHolder.finishWalk.setOnClickListener(View.OnClickListener { v ->
 
-    }
+            val documentReference = walksList.get(walksHolder.adapterPosition).getPostId()?.let { it -> docRef.collection("posts").document(it) }
+            documentReference?.update(
+                "status", "FINISHED"
+            )?.addOnCompleteListener(OnCompleteListener<Void?> { task ->
+                if (task.isSuccessful) {
+
+                    popUp = PopupWindow(ctx)
+                    val layout = LinearLayout(ctx)
+                    val mainLayout = LinearLayout(ctx)
+                    val gradeLabel = TextView(ctx)
+                    gradeLabel.setText("Select grade for walker")
+
+                    /*val fragment: Fragment = WalksFragment()
+                    val fragmentManager = (ctx as FragmentActivity).supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(
+                        R.id.walks_fragment,
+                        fragment
+                    )
+                    fragmentTransaction.addToBackStack(
+                        null
+                    )
+                    fragmentTransaction.commit()*/
+
+                    Toast.makeText(
+                        ctx,
+                        "Data successfully updated.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        ctx,
+                        "Error updating data.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })?.addOnFailureListener(OnFailureListener { })
+
+
+
+            /*
+
+            val but = Button(this)
+            but.text = "Click Me"
+            but.setOnClickListener(object : OnClickListener() {
+                fun onClick(v: View?) {
+                    if (click) {
+                        popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10)
+                        popUp.update(50, 50, 300, 80)
+                        click = false
+                    } else {
+                        popUp.dismiss()
+                        click = true
+                    }
+                }
+            })*/
+            })
+
+
+
+        }
+
+
 
     override fun getItemCount(): Int {
         return walksList.size
